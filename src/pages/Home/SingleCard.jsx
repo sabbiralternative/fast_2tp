54da8fbb933aad277eadbd3619c9bdfe02769084
sabbirHossain <sabbirshnt@gmail.com>
2025-08-23 12:23:55 +0600

    done

--- src/pages/Home/SingleCard.jsx
+++ src/pages/Home/SingleCard.jsx
@@ -18,6 +18,9 @@ const SingleCard = ({ style, card, highlight, player, winCard }) => {
     if (removeFirstCharacter == "11") {
       return "J";
     }
+    if (removeFirstCharacter == "1") {
+      return "A";
+    }
     return removeFirstCharacter;
   };
 
